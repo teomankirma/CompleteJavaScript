dbad032d27822e15c8278cc2b0c69349523982c9
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-06-30 18:11:09 +0300

    feat: functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -11,3 +11,27 @@ if (hasDriversLicence) console.log("I can drive :D");
 // const interface = "Audio";
 //const private = 534;
 */
+
+// Functions
+
+function logger() {
+    console.log("My name is Teoman");
+}
+
+// Calling / Running / Invoking Function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
