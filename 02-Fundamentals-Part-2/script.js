ddd082b96548c3b60c2b3d66fbefdbb39e5972a8
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-06-30 16:27:12 +0300

    Initial commit

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Activating Strict Mode
+/*
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive :D");
+
+// const interface = "Audio";
+//const private = 534;
+*/
