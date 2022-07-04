c66555e08c9f57121a62a9e70f6a2a85a870f550
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-07-04 11:52:31 +0300

    feat: introduction to arrays

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -128,3 +128,43 @@ function checkWinner(avgDolphins, avgKoalas) {
 
 checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
 */
+
+// Introduction to Arrays
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const firstName = "Teoman";
+const teoman = [firstName, "Kirma", 2022 - 2002, "student", friends];
+console.log(teoman);
+console.log(teoman.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
