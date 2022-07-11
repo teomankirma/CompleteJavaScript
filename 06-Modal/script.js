6c879b57e5ce2f3bf4f16c2000dcba0ed20ec301
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-07-11 14:10:55 +0300

    feat: handling an "esc" keypress event

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -22,3 +21,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
