b093b061c0de24eddc8c1351efc0e830edc5cfde
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-07-14 15:14:38 +0300

    feat: hoisting and TDZ in practice

--- 08-Behind-The-Scenes/script.js
+++ 08-Behind-The-Scenes/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Scoping in Practice
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -36,3 +38,49 @@ const firstName = "Teoman";
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Teoman";
+let job = "student";
+const year = 2002;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
