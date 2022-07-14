3344e2ffeaea646efb691c734ea9e8a7c59faa78
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-07-14 17:18:59 +0300

    feat: regular functions vs. arrow functions

--- 08-Behind-The-Scenes/script.js
+++ 08-Behind-The-Scenes/script.js
@@ -87,7 +87,7 @@ console.log(z === window.z);
 */
 
 // The this keyword in Practice
-
+/*
 // console.log(this);
 
 const calcAge = function (birthYear) {
@@ -120,3 +120,53 @@ matilda.calcAge();
 
 const f = teoman.calcAge;
 f();
+*/
+
+// Regular Functions vs. Arrow Functions
+
+// var firstName = "Matilda";
+
+const teoman = {
+  firstName: "Teoman",
+  year: 2002,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+teoman.greet();
+teoman.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
