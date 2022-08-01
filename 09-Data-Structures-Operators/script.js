5e00c0646506b3edf8fa18057f401d862ca9e90b
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-01 14:44:02 +0300

    feat: looping objects: object keys, values and entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -52,8 +52,34 @@ const restaurant = {
   },
 };
 
-// Optional Chaining (?.)
+// Looping Objects: Object Keys, Values and Entries
 
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+// Optional Chaining (?.)
+/*
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
 
@@ -81,6 +107,7 @@ console.log(users[0]?.name ?? "User array empty");
 
 if (users.length > 0) console.log(users[0].name);
 else console.log("user array empty");
+*/
 
 // Looping arrays: The for-of Loop
 /*
