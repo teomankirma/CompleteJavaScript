395040d07884503ac0cc37b3d7c586a7f9870c7c
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-01 18:50:26 +0300

    feat: working with strings - part 2

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -52,8 +52,64 @@ const restaurant = {
   },
 };
 
-// Working with Strings - Part 1
+// Working with Strings - Part 2
 
+const airline = "Turkish Airlines";
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = "tEoMaN"; // Teoman
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = "teomankirma@gmail.com";
+const loginEmail = "  Teomankirma@Gmail.Com \n";
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = "288,97￡";
+const priceUS = priceGB.replace("￡", "$").replace(",", ".");
+console.log(priceUS);
+
+const announcement =
+  "All passangers come to boarding door 23. Boarding door 23!";
+
+console.log(announcement.replaceAll("door", "gate"));
+
+// Booleans
+const plane = "Airbus A320neo";
+console.log(plane.includes("A320"));
+console.log(plane.includes("Boeing"));
+console.log(plane.startsWith("Airb"));
+
+if (plane.startsWith("Airbus") && plane.endsWith("neo"))
+  console.log("Part of the new Airbus family.");
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun"))
+    console.log("You are not allowed on board");
+  else console.log("Welcome aboard!");
+};
+
+checkBaggage("I have a laptop, some Food and a pocket knife.");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
+
+// Working with Strings - Part 1
+/*
 const airline = "Turkish Airlines";
 const plane = "A320";
 
@@ -93,6 +149,7 @@ console.log(new String("teoman"));
 console.log(typeof new String("teoman"));
 
 console.log(typeof new String("teoman").slice(1));
+*/
 
 // Coding Challenge #3
 /*
