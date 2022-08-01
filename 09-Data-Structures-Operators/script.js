54aeb8fc82358e774902e005ef8ba6a76db74e26
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-01 15:54:01 +0300

    feat: maps: fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -52,8 +52,41 @@ const restaurant = {
   },
 };
 
-// Sets
+// Maps: Fundamentals
 
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+// Sets
+/*
 const ordersSet = new Set([
   "Pasta",
   "Pizza",
@@ -87,6 +120,7 @@ console.log(
 );
 
 console.log(new Set("teomankirma").size);
+*/
 
 // Coding Challenge #2
 /*
