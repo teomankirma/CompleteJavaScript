5afb76d63398087708aec5cf453824dbefe30bab
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-01 11:57:42 +0300

    feat: coding challenge #1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,8 +54,76 @@ const restaurant = {
   },
 };
 
-// Logical Assignment Operators
+// Coding Challenge #1
 
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, ,] = [...game.players];
+const [, players2] = [...game.players];
+
+const [gk, ...fieldPlayers] = [...players1];
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...playerNames) {
+  console.log(playerNames);
+  console.log(`${playerNames.length} goals were scored.`);
+};
+
+printGoals(...game.scored);
+
+team1 < team2 && console.log("Team 1 is more likely to win");
+team2 < team1 && console.log("Team 2 is more likely to win");
+
+// Logical Assignment Operators
+/*
 const rest1 = {
   name: "Capri",
   // numGuests: 20,
@@ -85,6 +153,7 @@ rest2.owner &&= "<ANONYMOUS>";
 
 console.log(rest1);
 console.log(rest2);
+*/
 
 // The Nullish Coalescing Operator (??)
 /*
