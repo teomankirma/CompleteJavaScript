3d042abb9f883c49f1391661ab42d49fb8a0524d
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-02 15:23:37 +0300

    feat: how passing arguments works: values vs. reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Default Parameters
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +28,38 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000);
+*/
+
+// How Passing Arguments Works: Values vs. Reference
+
+const flight = "TK2310";
+const teoman = {
+  name: "Teoman Kirma",
+  passport: 1233241231413,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "TK2311";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 1233241231413) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+// checkIn(flight, teoman);
+// console.log(flight);
+// console.log(teoman);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = teoman;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(teoman);
+checkIn(flight, teoman);
