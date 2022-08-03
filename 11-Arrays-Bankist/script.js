6dfafca569e5c5066567383bac3a8b4168a1e7e7
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-03 18:49:36 +0300

    feat: coding challenge #3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -345,7 +345,7 @@ console.log(avg1, avg2);
 */
 
 // The Magic of Chaining Methods
-
+/*
 const euroToUsd = 1.1;
 console.log(movements);
 
@@ -359,3 +359,14 @@ const totalDepositsUSD = movements
   // .map((mov) => mov * euroToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
+
+// Coding Challenge #3
+const calcAverageHumanAge = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? age * 2 : age * 4 + 16))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
