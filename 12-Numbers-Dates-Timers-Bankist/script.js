b7e5f361be86556f9096d9755ae83d888e62c68b
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-07 17:54:23 +0300

    feat: timers: setTimeout and setInterval

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -283,14 +283,16 @@ btnLoan.addEventListener("click", function (e) {
     amount > 0 &&
     currentAccount.movements.some((mov) => mov >= amount * 0.1)
   ) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = "";
 });
@@ -543,7 +545,7 @@ console.log(days1);
 */
 
 // Internationalizing Numbers (Intl)
-
+/*
 const num = 3858385.23;
 
 const options = {
@@ -560,3 +562,29 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
+*/
+
+// Timers: setTimeout and setInterval
+
+// setTimeout
+const ingredients = ["olives", "spinach"];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log("Waiting...");
+
+if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(
+    new Intl.DateTimeFormat("tr-TR", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(now)
+  );
+}, 1000);
