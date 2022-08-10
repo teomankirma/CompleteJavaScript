e2d32886272c277d333e372454d9c37541c1bab1
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-10 13:43:57 +0300

    feat: implementing smooth scrolling

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -31,7 +31,7 @@ document.addEventListener("keydown", function (e) {
 });
 
 // Selecting, Creating and Deleting Elements
-
+/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -116,3 +116,38 @@ logo.classList.contains("c"); // not includes
 
 // Don't use
 logo.className = "teoman";
+*/
+
+// Implementing Smooth Scrolling
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scrool {X/Y}", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
