0d665d015ae14509857afa66a3c2678a68298e4d
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-10 15:18:35 +0300

    feat: event propagation in practice

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -153,7 +153,7 @@ btnScrollTo.addEventListener("click", function (e) {
 });
 
 // Types of Events and Event Handlers
-
+/*
 const h1 = document.querySelector("h1");
 
 const alertH1 = function (e) {
@@ -167,3 +167,31 @@ setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert("onmouseenter: Great! You are reading the heading :D");
 // };
+*/
+
+// Event Propagation in Practice
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("CONTAINER", e.target, e.currentTarget);
+});
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("NAV", e.target, e.currentTarget);
+});
