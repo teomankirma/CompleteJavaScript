608ce51b3293b9c816a7de7463c86ae4fd429a1c
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-12 18:21:09 +0300

    feat: lifecycle DOM events

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -474,3 +474,19 @@ console.log(h1.parentElement.children);
   if (el !== h1) el.style.transform = "scale(0.5)";
 });
 */
+
+// Lifecycle DOM Events
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree built!", e);
+});
+
+window.addEventListener("load", function (e) {
+  console.log("Page fully loaded", e);
+});
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// });
