460330429d83b72a22f6b9644705a41898a5c0c2
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-14 17:39:56 +0300

    feat: prototypal inheritance on built-in objects

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -50,3 +50,27 @@ console.log(teoman.species, matilda.species);
 
 console.log(teoman.hasOwnProperty("firstName"));
 console.log(teoman.hasOwnProperty("species"));
+
+// Prototypal Inheritance on Built-In Objects
+
+console.log(teoman.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(teoman.__proto__.__proto__);
+console.log(teoman.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
