b3b949ef6f768458a481c8151d4911763a938aec
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-14 16:39:57 +0300

    feat: constructor functions and the new operator

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+// Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const teoman = new Person("Teoman", 2002);
+console.log(teoman);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+
+console.log(teoman instanceof Person);
