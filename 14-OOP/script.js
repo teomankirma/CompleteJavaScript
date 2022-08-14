233424ab2f09e6813fe8bc778a5bada3932da731
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-14 16:56:47 +0300

    feat: prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -25,3 +25,28 @@ const jack = new Person("Jack", 1975);
 console.log(matilda, jack);
 
 console.log(teoman instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+teoman.calcAge();
+matilda.calcAge();
+
+console.log(teoman.__proto__);
+console.log(teoman.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(teoman));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(teoman.species, matilda.species);
+
+console.log(teoman.hasOwnProperty("firstName"));
+console.log(teoman.hasOwnProperty("species"));
