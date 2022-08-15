3764f5b63635ddb559cf098d7ddc0e0702c6e39c
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-15 18:21:41 +0300

    feat: encapsulation: protected properties and methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -431,28 +431,33 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -462,12 +467,13 @@ class Account {
 const acc1 = new Account("Teoman", "TRY", 1111);
 console.log(acc1);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
