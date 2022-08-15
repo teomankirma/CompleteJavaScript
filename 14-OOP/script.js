fb100f90bbb7e3ff12ca3382910deb207e493741
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-15 18:14:35 +0300

    feat: another class example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -395,6 +395,7 @@ martha.calcAge();
 */
 
 // Inheritance Between "Classes": Object.create
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -422,3 +423,51 @@ const jay = Object.create(StudentProto);
 jay.init("Jay", 2010, "Computer Science");
 jay.introduce();
 jay.calcAge();
+*/
+
+// Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account("Teoman", "TRY", 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
