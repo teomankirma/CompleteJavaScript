e7c7b126ac0258cd1c1fc6ebc98ed81fce78ed2f
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-15 17:12:47 +0300

    feat: coding challenge #3

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -256,7 +256,7 @@ console.log(ford);
 */
 
 // Inheritance Between "Classes": Constructor Functions
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -290,3 +290,43 @@ console.log(mike instanceof Person);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+// Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+
+EV.prototype.brake = function () {
+  this.speed -= 10;
+  console.log(
+    `${this.make} is going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate();
