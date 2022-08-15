2acf09da75b5c75616cdaec8e0ca11be2245588f
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-15 14:15:57 +0300

    feat: ES6 classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -78,7 +78,7 @@ console.dir((x) => x + 1);
 */
 
 // Coding Challenge #1
-
+/*
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -101,3 +101,41 @@ bmw.brake();
 const mercedes = new Car("Mercedes", 95);
 mercedes.accelerate();
 mercedes.brake();
+*/
+
+// ES6 Classes
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
