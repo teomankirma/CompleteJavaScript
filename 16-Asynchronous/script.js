58e5eee8c6b5f3057b3ed3bb647f66de5b04d440
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-18 17:44:02 +0300

    feat: the event loop in practice

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -215,7 +215,7 @@ btn.addEventListener("click", function () {
 // getCountryData("australia");
 
 // Coding Challenge #1
-
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then((res) => {
@@ -240,3 +240,17 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+// The Event Loop in Practice
+
+console.log("Test start");
+setTimeout(() => console.log("0 sec timer"), 0);
+Promise.resolve("Resolved promise 1").then((res) => console.log(res));
+
+Promise.resolve("Resolved promise 2").then((res) => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log("Test end");
