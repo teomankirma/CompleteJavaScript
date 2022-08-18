3340a00b6ff6984aec32f8a40a9b496184941ae7
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-18 15:00:09 +0300

    feat: chaining promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -134,9 +134,18 @@ console.log(request);
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data, "neighbour"));
 };
 
 getCountryData("portugal");
