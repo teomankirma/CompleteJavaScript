b8144dc7544f2ec3ad3bc7da853d2cb3d11e44fe
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-18 16:09:15 +0300

    feat: coding challenge #1

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -21,12 +21,12 @@ const renderCountry = function (data, className = "") {
       </article>
       `;
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 ///////////////////////////////////////
@@ -154,11 +154,11 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
 //     .then((response) => {
 //       console.log(response);
 
-//       if (!response.ok)
-//         throw new Error(`Country not found (${response.status})`);
+//   if (!response.ok)
+//     throw new Error(`Country not found (${response.status})`);
 
-//       return response.json();
-//     })
+//   return response.json();
+// })
 //     .then((data) => {
 //       renderCountry(data[0]);
 //       //   const neighbour = data[0].borders?.[0];
@@ -212,4 +212,31 @@ btn.addEventListener("click", function () {
   getCountryData("portugal");
 });
 
-getCountryData("australia");
+// getCountryData("australia");
+
+// Coding Challenge #1
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.log(`${err.message} 💥`));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
