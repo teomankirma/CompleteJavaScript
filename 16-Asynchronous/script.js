9c9a5c18a4c133865a3c4c66bfd6b9ce3a40e6f8
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-19 12:44:11 +0300

    feat: returning values from async functions

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -436,20 +436,18 @@ const whereAmI = async function () {
       `https://restcountries.com/v2/name/${dataGeo.country}`
     );
     if (!res.ok) throw new Error("Problem getting country");
-
     const data = await res.json();
-    console.log(data);
     renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
     console.error(`${err}`);
     renderError(`Something went wrong 💥 ${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 };
-whereAmI();
-whereAmI();
-whereAmI();
-
-console.log("FIRST");
 
 // try {
 //   let y = 1;
@@ -458,3 +456,22 @@ console.log("FIRST");
 // } catch (err) {
 //   alert(err.message);
 // }
+
+console.log("1: Will get location");
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then((city) => console.log(`2: ${city}`))
+//   .catch((err) => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log("3: Finished getting location"));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💥`);
+  }
+  console.log("3: Finished getting location");
+})();
