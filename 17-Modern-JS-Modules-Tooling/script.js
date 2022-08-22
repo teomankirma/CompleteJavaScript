c2ca292f41e26cddcf4e8ecceb0a5a252e7a0542
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-22 17:52:30 +0300

    feat: configuring babel and polyfilling

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -115,3 +115,24 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  greeting = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+const teoman = new Person("Teoman");
+
+console.log("Teoman" ?? null);
+
+console.log(cart.find((el) => el.quantity >= 2));
+Promise.resolve("TEST").then((x) => console.log(x));
+
+import "core-js/stable";
+// import "core-js/stable/array/find";
+// import "core-js/stable/promise";
+
+// Polyfilling async function
+import "regenerator-runtime/runtime";
