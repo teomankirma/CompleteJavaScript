c5ad87a49b2dce68bf91bb3c02274e4dded368ae
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-23 19:00:06 +0300

    feat: helpers and configuration files

--- 18-Forkify/src/js/model.js
+++ 18-Forkify/src/js/model.js
@@ -1,15 +1,13 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,6 +22,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    // Temp error handling
+    console.error(`${err} 💥💥💥💥`);
   }
 };
