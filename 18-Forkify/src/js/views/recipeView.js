266e8e8ea50721e79f6d936fd865ae627b24faef
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-27 17:23:08 +0300

    fix: quantity's were shown as object

--- 18-Forkify/src/js/views/recipeView.js
+++ 18-Forkify/src/js/views/recipeView.js
@@ -134,7 +134,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new fracty(ing.quantity).toString() : ""
+      ing.quantity ? fracty(ing.quantity).toString() : ""
     }</div>
     <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
