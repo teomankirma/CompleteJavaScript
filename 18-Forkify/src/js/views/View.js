2023165680a1247feac877aa44e7f2bbb253fbf6
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-08-27 11:38:20 +0300

    feat: render function documentation

--- 18-Forkify/src/js/views/View.js
+++ 18-Forkify/src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from "url:../../img/icons.svg"; // Parcel 2
 export default class View {
   _data;
 
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render = false
+   * @this {Object} View instance
+   * @author Teoman Kirma
+   * @todo Finish implementation
+   */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
